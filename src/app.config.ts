4f8230d4d123b0e2d20260bc94dfd042cc51b59e
Ubuntu <ubuntu@localhost.localdomain>
2026-04-08 00:28:36 +0800

    fix: add tabbar icons for wechat preview

--- src/app.config.ts
+++ src/app.config.ts
@@ -29,27 +29,32 @@ export default {
       {
         pagePath: 'pages/home/index',
         text: '首页',
-        iconPath: ''
+        iconPath: 'assets/tabbar/home.png',
+        selectedIconPath: 'assets/tabbar/home-active.png'
       },
       {
         pagePath: 'pages/discover/index',
         text: '发现',
-        iconPath: ''
+        iconPath: 'assets/tabbar/discover.png',
+        selectedIconPath: 'assets/tabbar/discover-active.png'
       },
       {
         pagePath: 'pages/publish/index',
         text: '发布',
-        iconPath: ''
+        iconPath: 'assets/tabbar/publish.png',
+        selectedIconPath: 'assets/tabbar/publish-active.png'
       },
       {
         pagePath: 'pages/events/index',
         text: '活动',
-        iconPath: ''
+        iconPath: 'assets/tabbar/events.png',
+        selectedIconPath: 'assets/tabbar/events-active.png'
       },
       {
         pagePath: 'pages/profile/index',
         text: '我的',
-        iconPath: ''
+        iconPath: 'assets/tabbar/profile.png',
+        selectedIconPath: 'assets/tabbar/profile-active.png'
       }
     ],
     selectedColor: '#4a0080',
